0cc79bf167dccdb8d167484c0a6c1161f4cabc7a
aspire51 <aspire51@naver.com>
2023-02-01 17:49:44 +0900

    sol

---
+++ baekjjon/boj_14890.java
@@ -0,0 +1,11 @@
+package gwonjihun.baekjjon;
+
+public class boj_14890 {
+	static int[][] map;
+	static int[][] visited;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
