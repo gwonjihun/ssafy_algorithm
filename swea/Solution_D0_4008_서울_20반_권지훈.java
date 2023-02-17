418ce356739265dc50160d165d024bde0f4c9488
aspire51 <aspire51@naver.com>
2023-02-17 17:42:01 +0900

    welcome weekday!

--- swea/Solution_D0_4008_서울_20반_권지훈.java
+++ swea/Solution_D0_4008_서울_20반_권지훈.java
@@ -23,38 +23,41 @@ public class Solution_D0_4008_서울_20반_권지훈 {
 			}
 
 			st = new StringTokenizer(br.readLine(), " ");
-			
+
 			arr = new int[N];
 			for (int i = 0; i < N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			dfs(1, arr[0], su[0], su[1], su[2], su[3]);
-			sb.append("#").append(T).append(" ").append(Max-Min).append("\n");
+			sb.append("#").append(T).append(" ").append(Max - Min).append("\n");
 		}
 		System.out.println(sb);
 	}
 
 	static void dfs(int cnt, int result, int add, int minus, int mul, int divs) {
 		if (cnt == N) {
-			Max = Math.max(Max, result);
-			Min = Math.min(Min, result);
-
+			if(Max <= result) {
+			Max =result;
+			}
+			if(Min >=result) {
+			Min = result;
+			}
 			return;
 
 		}
 		if (add > 0) {
-			dfs(cnt+1, result+arr[cnt],add-1,minus,mul,divs);
+			dfs(cnt + 1, result + arr[cnt], add - 1, minus, mul, divs);
 		}
 		if (minus > 0) {
-			dfs(cnt+1, result-arr[cnt],add,minus-1,mul,divs);
+			dfs(cnt + 1, result - arr[cnt], add, minus - 1, mul, divs);
 
 		}
 		if (mul > 0) {
-			dfs(cnt+1, result*arr[cnt],add,minus,mul-1,divs);
+			dfs(cnt + 1, result * arr[cnt], add, minus, mul - 1, divs);
 
 		}
 		if (divs > 0) {
-			dfs(cnt+1, result/arr[cnt],add,minus,mul,divs-1);
+			dfs(cnt + 1, result / arr[cnt], add, minus, mul, divs - 1);
 
 		}
 	}
