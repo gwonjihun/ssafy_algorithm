a16a708011a849f1dad1793ae5dacd30b176378d
gwonjihun <aspire51@naver.com>
2023-02-27 23:54:39 +0900

    !

---
+++ swea/Solution_D0_1953_서울_20반_권지훈.java
@@ -0,0 +1,15 @@
+package swea;
+
+public class Solution_D0_1953_서울_20반_권지훈 {
+    static int[] dx={-1,1,0,0},dy={0,0,1,-1};
+    static int[][] pipe = {
+            {},
+            {0,1,2,3},
+            {0,1},
+            {2,3},
+            {0,2},
+            {1,2},
+            {3,1},
+            {3,0}
+    };
+}
