7ea4d432c4fcb53feb6841c9730dae95ab47aaea
gwonjihun <aspire51@naver.com>
2023-03-03 17:34:19 +0900

    tomato get

---
+++ baekjjon/Main_boj_7576_토마토_서울_20반_권지훈.java
@@ -0,0 +1,49 @@
+package gwonjihun.baekjjon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_boj_7576_토마토_서울_20반_권지훈 {
+	static int[][] map;
+	static int N, M;
+	static int[] dx = {0,0,1,-1}, dy = {1,-1,0,0};
+	static Deque<int[]> q;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		q = new ArrayDeque<>();
+		map = new int[N][M];
+		for(int i = 0; i< N;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j = 0 ; j < M ; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j]==1) q.addLast(new int[] {i,j,0});
+			}
+		}
+		int answer = 0;
+		while(!q.isEmpty()) {
+			int[] xy = q.poll();
+			for(int d=0; d<4;d++) {
+				int nx = xy[0]+dx[d];
+				int ny = xy[1]+dy[d];
+				if(0<= nx && nx<N && 0<= ny && ny<M && map[nx][ny]==0) {
+					map[nx][ny] = xy[2]+1;
+					answer = Math.max(answer, map[nx][ny]);
+					q.addLast(new int[] {nx,ny,xy[2]+1});
+				}
+			}
+		}
+	
+		for(int i = 0; i< N;i++) {
+			for(int j = 0 ; j < M ; j++) {
+				if(map[i][j]==0) answer = -1;
+			}
+		}
+		for(int[] a: map) {
+			System.out.println(Arrays.toString(a));
+		}
+		System.out.println(answer);
+	}
+}
