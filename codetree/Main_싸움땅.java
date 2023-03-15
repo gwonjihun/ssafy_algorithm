7c5bcc6945c378a86b7f6a8f9e974d87fdbb3865
gwonjihun <aspire51@naver.com>
2023-03-15 17:58:39 +0900

    1

---
+++ codetree/Main_싸움땅.java
@@ -0,0 +1,27 @@
+package gwonjihun.codetree;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main_싸움땅 {
+	static int[] dx= {-1,0,1,0}, dy = {0,1,0,-1};
+	static class person{
+		int x,y,ablity;
+
+		@Override
+		public boolean equals(Object obj) {
+			person other = (person) obj;
+			if (x == other.x && y == other.y) {
+				return true;
+			}
+			return false;
+		}
+		
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+}
