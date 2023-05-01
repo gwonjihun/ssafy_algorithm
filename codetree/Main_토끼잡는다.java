bd38a972927a74558c08d01440f665a02734ec6c
hyunchul1998 <hyunchul1998@naver.com>
2023-05-01 12:27:28 +0900

    1

--- codetree/Main_토끼잡는다.java
+++ codetree/Main_토끼잡는다.java
@@ -2,16 +2,37 @@ package gwonjihun.codetree;
 
 import java.io.*;
 import java.util.*;
-
+/*
+1. 경주 격자가 필요한가?
+*/
 public class Main_토끼잡는다 {
+	
+	class ObjectSort implements Comparator{
 
-	static class Rabbit implements Comparable<Rabbit>{
-		int id, dist;
-		
 		@Override
-		public int compareTo(Rabbit o) {
+		public int compare(Object o1, Object o2) {
 			// TODO Auto-generated method stub
 			return 0;
 		}
+		
+	}
+	static class Rabbit {
+		int pid, dist, j_cnt, x,y;
+		
+		public Rabbit(int pid, int dist) {
+			this.pid= pid;
+			this.dist = dist;
+			this.j_cnt = 0;
+			this.j_cnt = 1;
+			this.j_cnt = 1;
+			// TODO Auto-generated constructor stub
+		}
+
+	}
+	
+	static int[] dx = {0,0,1,-1}, dy= {1,-1,0,0};
+	
+	public static void main(String[] args) throws Exception{
+		
 	}
 }
