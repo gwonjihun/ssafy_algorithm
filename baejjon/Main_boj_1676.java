04faa2abe141b9aa37d162270d8e3bdadea532c0
gwonjihun <aspire51@naver.com>
2023-07-09 23:47:40 +0900

    feat :에라스토 체

--- baejjon/Main_boj_1676.java
+++ baejjon/Main_boj_1676.java
@@ -1,5 +1,20 @@
 package gwonjihun.baejjon;
 
+import java.io.*;
+import java.util.*;
+
 public class Main_boj_1676 {
 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0;
+		while(N>=5) {
+			N= N/5;
+			sum += N;
+		}
+		System.out.println(sum);
+
+	}
 }
