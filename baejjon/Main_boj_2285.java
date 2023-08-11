561116817e83d8ae924a7db9aaa0bc30cd29cc35
gwonjihun <aspire51@naver.com>
2023-08-11 21:50:31 +0900

    feat : boj 2282

---
+++ baejjon/Main_boj_2285.java
@@ -0,0 +1,47 @@
+package gwonjihun.baejjon;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main_boj_2285 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		Node[] arr = new Node[N];
+		long sum = 0;
+		long cnt = 0;
+		for (int i = 0; i < N; i++) {
+			long x = sc.nextLong();
+			long a = sc.nextLong();
+			arr[i] = new Node(x, a);
+			cnt += arr[i].a;
+		}
+		Arrays.sort(arr, new Comparator<Node>() {
+			@Override
+			public int compare(Node n1, Node n2) {
+				if (n1.x == n2.x)
+					return (int) (n1.a - n2.a);
+				return (int) (n1.x - n2.x);
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			sum += arr[i].a;
+			if (sum >= (cnt + 1) / 2) {
+				System.out.println(arr[i].x);
+				break;
+			}
+		}
+	}
+
+	public static class Node {
+		long x;
+		long a;
+
+		public Node(long x2, long a2) {
+			super();
+			this.x = x2;
+			this.a = a2;
+		}
+	}
+}
