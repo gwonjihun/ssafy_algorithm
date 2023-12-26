8daa705a6145fd5ccdaa60e46d0c5723c7670f42
gwonjihun <aspire51@naver.com>
2023-12-26 21:44:08 +0900

    Feat : BOJ Algo Study

---
+++ baejjon/Main_boj_1025.java
@@ -0,0 +1,12 @@
+package gwonjihun.baejjon;
+
+import java.io.*;
+import java.util.*;
+/*
+ N개의 기존 점수와 새로운 점수, 랭킹에 올라갈 수 있는 점수의 개수
+ 1. 기존 점수를 arraylist에 넣는다?
+ 2. 일단 소팅을 해줘야한다.
+ */
+public class Main_boj_1025 {
+
+}
--- baejjon/Main_boj_1205.java
+++ baejjon/Main_boj_1205.java
@@ -2,34 +2,41 @@ package gwonjihun.baejjon;
 
 import java.io.*;
 import java.util.*;
+
 public class Main_boj_1205 {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int input = Integer.parseInt(st.nextToken());
 		int P = Integer.parseInt(st.nextToken());
-		int rank = 1;
-		int[] arr = new int[N];
+		Integer[] arr = new Integer[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N ; i++) {
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-	
-		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
-		for(int i= N-1 ; i >=0 ; i--) {
-			System.out.println(arr[i]);
-			if(arr[i]<=input) {
-				System.out.print(rank);
-				break;
-			}else {
-				rank++;
-				P--;
+
+		Arrays.sort(arr, Collections.reverseOrder());
+//		System.out.println(Arrays.toString(arr));
+		if (N == P && input <= arr[arr.length - 1]) {
+			System.out.println(-1);
+		} else {
+
+			int rank = 1;
+			for (int i = 0; i < N; i++) {
+//			System.out.println(arr[i]);
+				if (arr[i] > input) {
+					rank++;
+				} else {
+					break;
+//					P--;
+				}
 			}
+
+			System.out.print(rank);
 		}
 	}
 }
