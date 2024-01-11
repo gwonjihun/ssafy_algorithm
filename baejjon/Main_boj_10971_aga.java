d28b912bfbf6b6834f2baffa15e3ae61b09e43f5
gwonjihun <aspire51@naver.com>
2024-01-11 22:51:04 +0900

    Feat : Boj Algo Sol

--- baejjon/Main_boj_10971_aga.java
+++ baejjon/Main_boj_10971_aga.java
@@ -1,5 +1,28 @@
 package gwonjihun.baejjon;
 
+import java.io.*;
+import java.util.*;
 public class Main_boj_10971_aga {
 
+	static int[][] map;
+	static int[] route;
+	static boolean[] visited;
+	static int N;
+	static int min = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		route = new int[N];
+		map = new int[N][N];
+		visited= new boolean[N];
+		for(int i = 0 ; i < N ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0 ; j < N ; j++) {
+				
+			}
+		}
+		
+	}
+	
 }
