ecdeb19c9a602e658b8a5f0ba0c97c24c466d550
gwonjihun <aspire51@naver.com>
2024-02-03 23:20:00 +0900

    Feat : Code Tree 삼성 기출 Fail

--- codetree/Main_루돌프의반란.java
+++ codetree/Main_루돌프의반란.java
@@ -6,7 +6,8 @@ import java.util.StringTokenizer;
 
 public class Main_루돌프의반란 {
 	static int N,M,P,C,D;
-	static int
+	static int[] stun;//기절하게된 라운드값을 넣어준다?
+	static int[] score;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(),"-");
