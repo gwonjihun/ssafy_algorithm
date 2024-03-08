9948e7325a5ae89ebdf7c2780d58be9c4fe74a4b
gwonjihun <aspire51@naver.com>
2024-03-08 21:39:37 +0900

    Feat : CodeTree Algo

---
+++ codetree/Main_CodeTree_싸움땅.java
@@ -0,0 +1,28 @@
+package gwonjihun.codetree;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_CodeTree_싸움땅 {
+	static class Person{
+		int x,y,dir,gun,strength;
+
+		public Person(int x, int y, int dir, int strength) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+			this.strength = strength;
+			this.gun= 0;
+		}
+		
+		
+	}
+	static int N;
+	
+	
+	static Queue<Integer>[][] q;//총을 기록해놓기 위한 곳
+	public static void main(String[] args) throws Exception{
+		
+	}
+}
